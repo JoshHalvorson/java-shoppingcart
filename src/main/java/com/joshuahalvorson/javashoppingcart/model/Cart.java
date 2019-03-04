b621dd2d9dd19db5e2126626c893fcab83b668df
Josh Halvorson <joshhalvorson16@gmail.com>
2019-03-04 09:59:06 -0800

    Update cart class

--- src/main/java/com/joshuahalvorson/javashoppingcart/model/Cart.java
+++ src/main/java/com/joshuahalvorson/javashoppingcart/model/Cart.java
@@ -14,4 +14,23 @@ public class Cart {
     private long cart_id;
 
     private Set<Product> products = new HashSet<>();
+
+    public Cart() {
+    }
+
+    public long getCart_id() {
+        return cart_id;
+    }
+
+    public void setCart_id(long cart_id) {
+        this.cart_id = cart_id;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
 }
