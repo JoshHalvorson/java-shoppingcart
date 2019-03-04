383c119738f3db76b81fb885ff68990bc2049c4d
Josh Halvorson <joshhalvorson16@gmail.com>
2019-03-04 12:52:46 -0800

    Add method to get product by name

--- src/main/java/com/joshuahalvorson/javashoppingcart/Repository/ProductRepository.java
+++ src/main/java/com/joshuahalvorson/javashoppingcart/Repository/ProductRepository.java
@@ -4,4 +4,5 @@ import com.joshuahalvorson.javashoppingcart.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    public Product findByProductName(String name);
 }
--- src/main/java/com/joshuahalvorson/javashoppingcart/controller/ProductController.java
+++ src/main/java/com/joshuahalvorson/javashoppingcart/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.joshuahalvorson.javashoppingcart.Repository.ProductRepository;
 import com.joshuahalvorson.javashoppingcart.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,5 +17,16 @@ public class ProductController {
     @GetMapping("/products")
     public List<Product> getAllProducts(){
         return productRepository.findAll();
+    }
+
+    //TODO add product to cart by id
+    
+    @GetMapping("/products/{productname}")
+    public Product getProductByName(@PathVariable String productname){
+        Product foundProduct = productRepository.findByProductName(productname);
+        if(foundProduct != null){
+            return foundProduct;
+        }
+        return null;
     }
 }
