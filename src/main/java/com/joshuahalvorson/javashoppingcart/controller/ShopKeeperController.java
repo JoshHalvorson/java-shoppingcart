7a58fc153251ad93227266cd26d4c12acd2001e2
Josh Halvorson <joshhalvorson16@gmail.com>
2019-03-05 10:30:03 -0800

    Add method to update product by id

--- src/main/java/com/joshuahalvorson/javashoppingcart/controller/ShopKeeperController.java
+++ src/main/java/com/joshuahalvorson/javashoppingcart/controller/ShopKeeperController.java
@@ -5,9 +5,10 @@ import com.joshuahalvorson.javashoppingcart.Repository.ProductRepository;
 import com.joshuahalvorson.javashoppingcart.Repository.SupplierRepository;
 import com.joshuahalvorson.javashoppingcart.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URISyntaxException;
+import java.util.Optional;
 
 @RestController
 public class ShopKeeperController {
@@ -24,4 +25,16 @@ public class ShopKeeperController {
     public Product addProduct(@RequestBody Product product){
         return productRepository.save(product);
     }
+
+    @PutMapping("/shopkeeper/product/{productid}")
+    public Product updateProductById(@RequestBody Product newProduct, @PathVariable long productid) throws URISyntaxException {
+        Optional<Product> productToUpdate = productRepository.findById(productid);
+        if (productToUpdate.isPresent()) {
+            newProduct.setProductId(productid);
+            productRepository.save(newProduct);
+            return newProduct;
+        }
+        return null;
+    }
+    
 }
