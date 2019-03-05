34420dad93c45f706bae27f5b9f4b91c985237b0
Josh Halvorson <joshhalvorson16@gmail.com>
2019-03-05 10:50:28 -0800

    Add method to update shopper by id

---
+++ src/main/java/com/joshuahalvorson/javashoppingcart/controller/AdminController.java
@@ -0,0 +1,31 @@
+package com.joshuahalvorson.javashoppingcart.controller;
+
+import com.joshuahalvorson.javashoppingcart.Repository.ShopperRepository;
+import com.joshuahalvorson.javashoppingcart.model.Product;
+import com.joshuahalvorson.javashoppingcart.model.Shopper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Optional;
+
+@RestController
+public class AdminController {
+    @Autowired
+    ShopperRepository shopperRepository;
+
+    @PutMapping("/admin/shopper/{shopperid}")
+    public Shopper updateShopper(@RequestBody Shopper newShopper, @PathVariable long shopperid) throws URISyntaxException{
+        Optional<Shopper> shopperToUpdate = shopperRepository.findById(shopperid);
+        if (shopperToUpdate.isPresent()) {
+            newShopper.setShopperId(shopperid);
+            shopperRepository.save(newShopper);
+            return newShopper;
+        }
+        return null;
+    }
+}
