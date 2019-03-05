e9e440db4f293558f8c25dc14d1ca43d104befeb
Josh Halvorson <joshhalvorson16@gmail.com>
2019-03-05 10:45:18 -0800

    Add method to add a new shopper

---
+++ src/main/java/com/joshuahalvorson/javashoppingcart/controller/ShopperController.java
@@ -0,0 +1,19 @@
+package com.joshuahalvorson.javashoppingcart.controller;
+
+import com.joshuahalvorson.javashoppingcart.Repository.ShopperRepository;
+import com.joshuahalvorson.javashoppingcart.model.Shopper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ShopperController {
+    @Autowired
+    ShopperRepository shopperRepository;
+
+    @PostMapping("/shopper")
+    public Shopper addShopper(@RequestBody Shopper shopper){
+        return shopperRepository.save(shopper);
+    }
+}
